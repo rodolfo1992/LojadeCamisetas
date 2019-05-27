1c3b98604eb43537ca5c19fa97ad0831b3c1399e
Rodolfo Douglas <rod_doug1@hotmail.com>
2019-05-27 17:28:10 -0300

    modificações

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post "/produtos" => "produtos#create"
-  get "/produtos/new" => "produtos#new"
-  delete "/produtos/:id" => "produtos#destroy", as: :produto
-  root "produtos#index"
+ resources :produtos, only: [:new, :create, :destroy]
+ root "produtos#index"
+ # post "/produtos" => "produtos#create"
+ # get "/produtos/new" => "produtos#new"
+ # delete "/produtos/:id" => "produtos#destroy", as: :produto
+ # root "produtos#index"
 end
