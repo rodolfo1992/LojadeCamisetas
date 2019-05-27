9ce6bbea034c0144841c24aedfc2c2751b57251a
Rodolfo Douglas <rod_doug1@hotmail.com>
2019-05-27 16:39:48 -0300

    Aula 09: Alterações na estrutura do arquivo para remover direto pela a interface. Aplicado via metodo destroy no Controller

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,11 +1,18 @@
 class ProdutosController < ApplicationController
 	def index
-		@produtos_por_nome = Produto.all.order(:nome).limit 20
-		@produtos_por_preco = Produto.all.order(:preco).limit 20
+		@produtos_por_id = Produto.all.order(:id).limit 10
+		@produtos_por_preco = Produto.all.order(:preco).limit 10
 	end
 
 	def create
 		valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
 		produto = Produto.create valores
+		redirect_to root_url
+	end
+
+	def destroy 
+		id = params[:id]
+		Produto.destroy id
+		redirect_to root_url
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "/produtos" => "produtos#create"
   get "/produtos/new" => "produtos#new"
+  delete "/produtos/:id" => "produtos#destroy", as: :produto
   root "produtos#index"
 end
