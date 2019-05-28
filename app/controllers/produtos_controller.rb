a7f491b93a31eea4aa392e1be7c98cbc6e8098a1
Rodolfo Douglas <rod_doug1@hotmail.com>
2019-05-28 19:44:11 -0300

    Aula 11: Adição da internacionalização para nome: quantidade: preco: + inclusão do render new e correção do redirect com flash[:notice]

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -3,16 +3,23 @@ class ProdutosController < ApplicationController
 		@produtos_por_id = Produto.all.order(:id).limit 10
 		@produtos_por_preco = Produto.all.order(:preco).limit 10
 	end
-
+	def new
+		@produto = Produto.new
+	end
 	def busca
 		nome = "%#{params[:nome]}%"
 		@produtos = Produto.where "nome like ?", nome
 	end
 	def create
 		valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
-		produto = Produto.create valores
+		@produto = Produto.new valores
+		if @produto.save
+			@mensagem = "Produto Salvo com sucesso"
 		redirect_to root_url
+	else	
+		render :new
 	end
+end
 
 	def destroy 
 		id = params[:id]
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,5 @@
 class Produto < ApplicationRecord
+	validates :quantidade, presence: true
+	validates :preco, presence: true
+	validates :nome, presence: true
 end
